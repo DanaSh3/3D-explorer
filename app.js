a4e61b5e3df8933bc422e370b93e81079eae50e3
Aibek Sarbayev <aibek.asm@gmail.com>
2013-11-10 08:21:20 -0500

    Merge branch 'master' of E:\Aibek\My homework\Fall 2013\CS 326\3Dexplorer\master with conflicts.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-var fs = require('fs');
 
 var app = express();
 
@@ -26,89 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+  app.use(express.errorHandler());
 }
 
 app.get('/3dexplorer', routes.index);
 
-var io = require('socket.io').listen(app.listen(app.get('port')));
-
-/*http.createServer(app).listen(app.get('port'), function(){
- console.log('Express server listening on port ' + app.get('port'));
- });*/
-
-io.sockets.on('connection', function (socket) {
-    console.log("Web Socket Connection Established");
-    //MAKE DIRECTORY
-    socket.on('mkdir', function (data) {
-        var path = getPath(data.dir);
-        data.isError = false;
-        if (file_exists(path)){
-            data.status = data.dir + " already exists";
-        }else{
-            data.status = "Successfully created " + data.dir;
-            fs.mkdir(path, 0777, function(err){
-                if(err){
-                    data.isError = true;
-                    data.status = "Failed to create " + data.dir;
-                }
-            });
-        }
-        //data.isDir = fs.statSync(path).isDirectory(); //determine if directory or file
-        socket.emit('status', data);
-    });
-    //SHOW DIRECTORY
-    socket.on('showdir', function(data){
-        var path = getPath(data.dir);
-        if (!file_exists(path)){
-            return;
-        }
-        if (!fs.lstatSync(path).isDirectory()){
-            return;
-        }
-        var files = fs.readdirSync(path);
-
-        var isDir = [];
-        //check each file/folder to determine if directory
-        for (var i = 0; i < files.length; i++){
-            isDir[i] = fs.lstatSync(path + "/" + files[i]).isDirectory();
-        }
-        data.files = files;
-        data.isDir = isDir;
-        io.sockets.emit('showfiles', data);
-    });
-    function getPath(filename){
-        var path = String(filename);
-        path = path.replace(/\//g, '\\');
-        path = __dirname + "\\public" + path;
-        return path;
-    }
-    //DELETE FOLDER/FILE
-    socket.on('deletedir', function(data){
-        var path = getPath(data.dir);
-        deleteFolder(path);
-        data.isError = false;
-        data.status = "Deleted " + data.dir;
-        socket.emit('status', data);
-    });
-    //DOES FILE EXIST?
-    function file_exists(file){
-        return fs.existsSync(file);
-    }
-    //DELETE FOLDER & CONTENTS
-    function deleteFolder(path) {
-        var files = [];
-        if(file_exists(path)) {
-            files = fs.readdirSync(path);
-            files.forEach(function(file,index){
-                var curPath = path + "/" + file;
-                if(fs.statSync(curPath).isDirectory()) { // recursive call
-                    deleteFolder(curPath);
-                } else { // delete file
-                    fs.unlinkSync(curPath);
-                }
-            });
-            fs.rmdirSync(path);
-        }
-    };
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
--- public/javascripts/hierarchy.js
+++ public/javascripts/hierarchy.js
@@ -1,53 +1,12 @@
 /**
  * Created by Oscar on 11/9/13.
  */
-
-//display a fancy success/error message in the side menu which will fade in and out:
-var timer;
-var statusMessage = function(text, errMessage){
-    var message = $('#message');
-    message.html(text);
-    if (errMessage){
-        message.addClass('errorMessage');
-        message.removeClass('statusMessage');
-    }else{
-        message.removeClass('errorMessage');
-        message.addClass('statusMessage');
-    }
-    if (timer)
-        clearTimeout(timer);
-    message.fadeIn(1000, "swing", new function(){
-        timer = setTimeout(function(){
-            message.fadeOut(1500, "swing", function(){
-                return;
-            });
-        }, 1750);
+function initClick(){
+    $('.h_node').click(function(e){
+        var path = this.id;
+        $('[id*=' + path + ']').each(function(){
+            //this.remove();
+            console.log(this.id);
+        });
     });
 }
-
-function rightClick(){
-    $(".h_node").on('contextmenu', function(ev) {
-        var that = this;
-        var menu = $(".custom-menu");
-        menu.show();
-        menu.css({top: event.pageY + "px", left: event.pageX + "px"});
-        $(menu).click(function(){
-            if (that.id == "/home"){
-                statusMessage("Cannot delete HOME", true);
-                return false;
-            }
-            var o = $('.h_node')[$(that).index()-1];
-            if(o == undefined) return; //because multiple calls are being made to the method
-            var id = that.id;
-            deleteDir(id); //delete the selected file
-            navigate(o); //navigate to the selected file
-            $('#file_input').val('');
-
-        });
-        return false;
-    });
-    $(document).bind("click", function(event) {
-        $("div.custom-menu").hide();
-    });
-
-};
